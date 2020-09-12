cba51539cb74cdd7b5601e47449cb9539efea5ab
Evandro Capovilla <evandrocapovillajr@gmail.com>
2020-09-12 18:13:23 -0300

    program name updated

--- Program/crypto_aelpp.c
+++ Program/crypto_aelpp.c
@@ -19,13 +19,13 @@ static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
 int main(){
    int ret, fd;
    char stringToSend[BUFFER_LENGTH];
-   printf("Starting device test code example...\n");
-   fd = open("/dev/ebbchar", O_RDWR);             // Open the device with read/write access
+   printf("Starting device crypto_aelpp...\n");
+   fd = open("/dev/crypto_aelpp", O_RDWR);             // Open the device with read/write access
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
    }
-   printf("Type in a short string to send to the kernel module:\n");
+   printf("Type in a short string to send to the crypto_aelpp module:\n");
    scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
    printf("Writing message to the device [%s].\n", stringToSend);
    ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
